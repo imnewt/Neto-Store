2977b3413084df0b46349e14805249004108878b
Kibuto <nguyentrongnhanc8@gmail.com>
2020-01-04 11:49:17 +0700

    add to feature 'nothing here' if cart not anything

--- neto-store/src/pages/Cart.js
+++ neto-store/src/pages/Cart.js
@@ -5,55 +5,53 @@ import { CartContext } from "../contexts/Cart";
 import './Cart.css';
 
 export default class Cart extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <Table>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <CartContext.Consumer>
-            {({ cartItems, countIncrease, countDecrease }) =>
-              cartItems.map(value => (
+      <CartContext.Consumer>
+        {
+          ({ cartItems, countIncrease, countDecrease, sum }) => (
+            cartItems.length ? <Table>
+              <thead>
                 <tr>
-                  <td>
-                    <img
-                      src={value.imageUrl}
-                      alt="Book Image"
-                      width="50"
-                      height="70"
-                    />
-                  </td>
-                  <td className="font-weight-500">{value.name}</td>
-                  <td>
-                    <Button outline color="info" className="mr-3" onClick={() => countDecrease(value)}>-</Button>
-                     {value.count} 
-                    <Button outline color="info" className="ml-3" onClick={() => countIncrease(value)}>+</Button>
-                  </td>
-                  <td>{(Math.round((value.count * value.price) * 10)/10)}</td>
+                  <th style={{ textAlign: "center" }}>Image</th>
+                  <th style={{ textAlign: "center" }}>Name</th>
+                  <th style={{ textAlign: "center" }}>Quantity</th>
+                  <th style={{ textAlign: "center" }}>Price</th>
                 </tr>
-              ))
-            }
-          </CartContext.Consumer>
-          <tr>
-            <th colSpan="3" style={{ textAlign: "center" }}>
-              Total
-            </th>
-            <CartContext.Consumer>
-              {item => <th>${item.sum}</th>}
-            </CartContext.Consumer>
-          </tr>
-        </tbody>
-      </Table>
+              </thead>
+              <tbody>
+                {
+                  cartItems.map(value => (
+                    <tr>
+                      <td>
+                        <img
+                          src={value.imageUrl}
+                          alt="Book Image"
+                          width="50"
+                          height="70"
+                        />
+                      </td>
+                      <td style={{ textAlign: "center" }} className="font-weight-500">{value.name}</td>
+                      <td style={{ textAlign: "center" }}>
+                        <Button outline color="info" className="mr-3" onClick={() => countDecrease(value)}>-</Button>
+                        <span>{value.count}</span> 
+                        <Button outline color="info" className="ml-3" onClick={() => countIncrease(value)}>+</Button>
+                      </td>
+                      <td style={{ textAlign: "center" }}>{(Math.round((value.count * value.price) * 10)/10)}</td>
+                    </tr>
+                  ))
+                }
+                <tr>
+                  <th colSpan="3" style={{ textAlign: "center" }}>
+                    Total
+                  </th>
+                  <th style={{ textAlign: "center" }}>${sum}</th>
+                </tr>
+              </tbody>
+            </Table> : <div style={{ textAlign: "center" }}>Nothing here</div>
+          )
+        }
+      </CartContext.Consumer>
     );
   }
 }
